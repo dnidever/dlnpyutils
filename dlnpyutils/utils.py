cb65fdafe7faad2d33cb7246c69222e252c95607
David Nidever <dnidever@lsst.org>
2019-10-08 20:04:52 -0600

    Updates to utils.py

--- dlnpyutils/utils.py
+++ dlnpyutils/utils.py
@@ -753,10 +753,10 @@ def gsmooth(data,fwhm,mask=None,boundary='extend',fill=0.0,truncate=4.0,squared=
     if xsize % 2 == 0: xsize+=1   # must be odd
     g = Gaussian1DKernel(stddev=fwhm/2.35,x_size=xsize)
     if squared is False:
-        return convolve(data, g, mask=mask, boundary=boundary, fill_value=fill)
+        return convolve(data, g.array, mask=mask, boundary=boundary, fill_value=fill)
         #return gaussian_filter1d(data,fwhm/2.35,axis=axis,mode=mode,cval=cval,truncate=truncate)
     else:
-        return convolve(data, g**2, mask=mask, boundary=boundary, fill_value=fill)
+        return convolve(data, g.array**2, mask=mask, boundary=boundary, fill_value=fill, normalize_kernel=False)
         #return gaussian_filter1d(data,fwhm/2.35,axis=axis,mode=mode,cval=cval,truncate=truncate)**2
     
 # Rebin data
