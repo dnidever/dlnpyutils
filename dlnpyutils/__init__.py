547f0f8914a08b3854a66626bb7e805aebbee214
David Nidever <dnidever@lsst.org>
2021-02-16 12:33:09 -0700

    Adding modified version of curve_fit.

--- dlnpyutils/__init__.py
+++ dlnpyutils/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ["utils", "coords", "bindata", "job_daemon", "gaps", "astro", "plotting", "db", "spec"] 
+__all__ = ["utils", "coords", "bindata", "job_daemon", "gaps", "astro", "plotting", "db", "spec","minpack","least_squares","trf"] 
