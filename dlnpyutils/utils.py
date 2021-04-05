7f881ec567fed79dde2baaf93046d8f6dfd6004a
David Nidever <dnidever@lsst.org>
2021-04-04 20:37:34 -0600

    Adding fread() in utils.py

--- dlnpyutils/utils.py
+++ dlnpyutils/utils.py
@@ -1704,3 +1704,60 @@ def ladfit(xin, yin):
     absdev = absdev / nx
 
     return np.array([aa, bb],float), absdev
+
+
+def fread(line,fmt):
+    """
+    Read the values in a string into variables using a format string.
+    (1X, A8, 4I5, F9.3, F15.3, 2F9.1)
+    """
+    # Transform the format string into an array
+    if fmt.startswith('('):
+        fmt = fmt[1:]
+    if fmt.endswith(')'):
+        fmt = fmt[:-1]
+    fmtarr = fmt.split(',')
+    # Expand repeat values, e.g. 2I5 -> I5,I5
+    fmtlist = []
+    for i in range(len(fmtarr)):
+        fmt1 = fmtarr[i].strip()
+        # X format starts with a number
+        if fmt1.find('X')>-1:
+            fmtlist.append(fmt1)
+            continue
+        # Repeats
+        if fmt1[0].isnumeric():
+            ind, = np.where(np.char.array(list(fmt1)).isalpha()==True)
+            ind = ind[0]
+            num = int(fmt1[0:ind])
+            
+            fmtlist += list(np.repeat(fmt1[ind:],num))
+        else:
+            fmtlist.append(fmt1)
+    # Start the output tuple
+    out = ()
+    count = 0
+    for i in range(len(fmtlist)):
+        fmt1 = fmtlist[i]
+        # Ignore X formats
+        if fmt1.find('X')==-1:
+            if fmt1[0]=='A':
+                num = int(fmt1[1:])
+                out1 = line[count:count+num+1]
+            elif fmt1[0]=='I':
+                num = int(fmt1[1:])
+                out1 = int(line[count:count+num+1])
+            else:
+                ind = fmt1.find('.')
+                num = int(fmt1[1:ind])
+                out1 = float(line[count:count+num+1])
+            out = out + (out1,)
+            count += num
+            
+        # X formats, increment the counter
+        else:
+            ind = fmt1.find('X')
+            num = int(fmt1[0:ind])
+            count += num
+
+    return out
