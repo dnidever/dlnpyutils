bdabdeda83de7213bc80ec76b5cb1b93d01e2f20
David Nidever <dnidever@lsst.org>
2021-07-10 21:04:40 -0600

    Updated __init__.py

--- dlnpyutils/__init__.py
+++ dlnpyutils/__init__.py
@@ -1,3 +1,3 @@
 __all__ = ["utils", "coords", "bindata", "job_daemon", "gaps", "astro", "plotting", "db",
-           "spec","robust","minpack","least_squares","trf"] 
+           "spec","ladfit","robust","minpack","least_squares","trf"] 
 __version__ = '1.0.3'
