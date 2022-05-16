c1d459bc1c5e802334aa5593a015a8d871707a42
David Nidever <dnidever@lsst.org>
2022-05-16 16:47:29 -0600

    bumped version from 1.0.31 to 1.0.32

--- dlnpyutils/__init__.py
+++ dlnpyutils/__init__.py
@@ -1,3 +1,3 @@
 __all__ = ["utils", "coords", "bindata", "job_daemon", "gaps", "astro", "plotting", "db",
            "spec","ladfit","robust","minpack","least_squares","trf","galaxy_model","lsqr"] 
-__version__ = '1.0.31'
+__version__ = '1.0.32'
