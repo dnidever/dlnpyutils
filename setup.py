2260ee8d5e48c9bfc0a2fb4bb9f8bd698e8f66bc
David Nidever <dnidever@lsst.org>
2022-12-28 19:39:18 -0700

    bumped version from 1.0.52 to 1.0.53

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name='dlnpyutils',
-      version='1.0.52',
+      version='1.0.53',
       description='David Nidever Python Utility Functions',
       author='David Nidever',
       author_email='dnidever@montana.edu',
