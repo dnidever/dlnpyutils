f3ad3d4e3eaedb0db030968ce6a6d187126b2ac3
David Nidever <dnidever@montana.edu>
2025-02-24 09:27:44 -0700

    bumped version from 1.0.68 to 1.0.69

--- dlnpyutils/__init__.py
+++ dlnpyutils/__init__.py
@@ -1,4 +1,4 @@
 __all__ = ["utils", "coords", "bindata", "job_daemon", "gaps", "astro", "plotting", "db",
            "spec","ladfit","robust","minpack","least_squares","trf","galaxy_model","lsqr",
            "mmm","thumbnail","slurm","resample","decompose","distribution","email"]
-__version__ = '1.0.68'
+__version__ = '1.0.69'
